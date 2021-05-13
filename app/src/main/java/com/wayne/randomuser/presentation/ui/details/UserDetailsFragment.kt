b8d57f8889aa8ffea22cdae5c73b21e663c06160
YKW93 <rhkddn1657@gmail.com>
2021-05-13 17:21:56 +0900

    feat: 사용자 상세 UI 작업

--- app/src/main/java/com/wayne/randomuser/presentation/ui/details/UserDetailsFragment.kt
+++ app/src/main/java/com/wayne/randomuser/presentation/ui/details/UserDetailsFragment.kt
@@ -1,17 +1,12 @@
 package com.wayne.randomuser.presentation.ui.details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
 import com.wayne.library.base.BaseFragment
 import com.wayne.randomuser.R
 import com.wayne.randomuser.databinding.FragmentUserDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
 
 @AndroidEntryPoint
 class UserDetailsFragment : BaseFragment<FragmentUserDetailsBinding, UserDetailsViewModel>(
--- app/src/main/java/com/wayne/randomuser/presentation/ui/details/UserDetailsViewModel.kt
+++ app/src/main/java/com/wayne/randomuser/presentation/ui/details/UserDetailsViewModel.kt
@@ -1,10 +1,30 @@
 package com.wayne.randomuser.presentation.ui.details
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.wayne.library.base.BaseViewModel
+import com.wayne.library.utils.BackDirections
+import com.wayne.randomuser.presentation.model.UserPresentation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UserDetailsViewModel @Inject constructor() : BaseViewModel() {
 
+    private val _userPresentation = MutableLiveData<UserPresentation>()
+    val userPresentation = _userPresentation
+
+    init {
+        viewModelScope.launch {
+            navArgs<UserDetailsFragmentArgs>().collect {
+                _userPresentation.value = it.user
+            }
+        }
+    }
+
+    fun goToBack() {
+        navDirections.value = BackDirections()
+    }
 }
